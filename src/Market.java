722c434b226e7e48635a5d1395f659dfb17ae327
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-22 21:56:48 +0900

    Feat :: Create addItem Logic
    아이템 설정 로직 추가

--- src/Market.java
+++ src/Market.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Market {
+    private ArrayList arr = new ArrayList();
+
+    public void addItem(String item){
+        this.arr.add(item);
+        System.out.println(this.arr);
+    }
 }
