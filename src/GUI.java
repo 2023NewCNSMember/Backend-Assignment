f1ef888497ffba05c18d47fcfcc91d827d8dc14b
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-22 21:55:54 +0900

    Feat :: Create GUI Logic
    코드 가독성을 위하여 println문 따로 저장

---
+++ src/GUI.java
@@ -0,0 +1,22 @@
+public class GUI {
+
+
+    public void roleGUI() {
+        System.out.println("----- Role -----");
+        System.out.println("1. ADMIN");
+        System.out.println("2. USER");
+    }
+
+    public void martGUI(){
+        System.out.println("-----대소고 마켓-----");
+        System.out.println("1. 물건 구매");
+        System.out.println("2. 코인 잔액 출력");
+        System.out.println("3. 물건 구매");
+    }
+
+    public void adminGUI(){
+        System.out.println("-----어드민 페이지-----");
+        System.out.println("1. 물건 추가");
+        System.out.println("2. 코인 설정");
+    }
+}
