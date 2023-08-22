2fb912f1cc019fa09fbaedbceb548fb285532f2b
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-22 22:03:03 +0900

    Refactor :: Add exit, Permission setting GUI
    exit, permission setting GUI 추가

--- src/GUI.java
+++ src/GUI.java
@@ -2,7 +2,7 @@ public class GUI {
 
 
     public void roleGUI() {
-        System.out.println("----- Role -----");
+        System.out.println("----- 권한설정 -----");
         System.out.println("1. ADMIN");
         System.out.println("2. USER");
     }
@@ -12,11 +12,15 @@ public class GUI {
         System.out.println("1. 물건 구매");
         System.out.println("2. 코인 잔액 출력");
         System.out.println("3. 물건 구매");
+        System.out.println("4. 권한 설정");
+        System.out.println("4. 나가기 (종료)");
     }
 
     public void adminGUI(){
         System.out.println("-----어드민 페이지-----");
         System.out.println("1. 물건 추가");
         System.out.println("2. 코인 설정");
+        System.out.println("3. 권한 설정");
+        System.out.println("4. 나가기 (종료)");
     }
 }
