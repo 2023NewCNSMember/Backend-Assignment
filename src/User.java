d96b2972f640019e52a8d91ceb415474e90967d6
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-08-23 08:04:09 +0900

    Feat :: Add Coin Logic
    getCoin : 코인 값 반환
    setCoin : 코인 값 설정

--- src/User.java
+++ src/User.java
@@ -1,8 +1,14 @@
-public class User {
+import java.util.List;
+
+public class User{
 
     private int coin;
     public void setCoin(int coin){
         this.coin = coin;
+    }
+
+    public int getCoin() {
+        return coin;
     }
 
 }
